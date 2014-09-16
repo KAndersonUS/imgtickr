97616ed82a3cc89e45bff7dcc4d8aa0e7a84101d
Kyle Anderson <mail@kyleanderson.us>
2014-09-15 22:43:56 -0400

    Added channels, styling, some safeguards and checks.

--- app.js
+++ app.js
@@ -14,16 +14,38 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var channels = {};
+var connections = {};
 
-io.on('connection', function (socket) {
-	console.log('socket connected');
-});
 
 app.engine('jade', require('jade').__express);
 //app.use(express.logger('dev'));
 app.set('view engine', 'jade');
 app.use(logger());
 app.use(express.static(__dirname));
+
+app.use(function (req, res, next) {
+	// limit concurrent IP requests to something reasonable, like 50 req/sec
+	var ip = req.ip.replace(/\./g, "");
+	if (connections.hasOwnProperty(ip)) {
+		connections[ip].count += 1;
+	} else {
+		connections[ip] = {
+			count : 1
+		};
+		var check = setInterval(function () {
+			connections[ip].count--;
+			if (connections[ip].count <= 0) {
+				delete connections[ip];
+				clearInterval(check);
+			}
+		},1000);
+	}
+	if (connections[ip].count > 50) {
+		res.send(403, "Too many concurrent connections from your IP address, " + req.ip);
+	} else {
+		next();
+	}
+});
 
 app.get('/', function (req, res, next) {
 	res.sendfile('./index.html');
@@ -44,118 +66,176 @@ app.get('/open/:channel', function (req, res, next) {
 	var channel = req.params.channel;
 	if (!channels.hasOwnProperty(channel)) {
 		// if channel doesn't exist, create it.
-		channels[channel] = {
-			socket : null,
-			members : 0
-		};
-		channels[channel].socket = io.of("/channels/" + channel);
-		channels[channel].members = 1;
-	} else {
-		channels[channel].members++;
+		openChannel(channel);
 	}
-	res.send(200);
+	res.send(200, {members:channels[channel].members.length+1});
 });
 
 app.get('/close/:channel', function (req, res, next) {
 	var channel = req.params.channel;
-	channels[channel].members--;
-	if (channels[channel].members <= 0) {
+	var socketId = req.param('socketId');
+	if (channels[channel].members.length <= 0) {
 		delete channels[channel];
 	}
 	res.send(200);
 });
 
+app.get('/stats/:area', function (req, res, next) {
+
+});
+
 app.post('/upload/:channel', function (req, res, next) {
 	var form = new formidable.IncomingForm();
 	var channel = req.param('channel');
+
 	form.parse(req, function (err, fields, files) {
 		if (err) {
 			console.log(err);
 			res.writeHead(503, err);
 			res.end();
 		} else {
-//			console.log(files);
-			for (file in files) {
-				processImage(files[file].path, function (err, encLine) {
-					if (err) { console.log(err); }
-					else {
-						channels[channel].socket.emit(channel, encLine);
-					}
-				});
+			if (fields.handle && fields.handle.length == 13) {
+				for (file in files) {
+					processImage(files[file].path, fields.handle, function (err, encLine) {
+						if (err) { console.log(err); }
+						else {
+							if (!channels.hasOwnProperty(channel)) {
+								openChannel(channel);
+							}
+							channels[channel].socket.emit(channel + "line", encLine);
+						}
+					});
+				}
+				res.writeHead(200);
+				res.end();
+			} else {
+				res.send(403, "Bad handle");
 			}
-			res.writeHead(200);
-			res.end();
+
+
 		}
 	});
+
 });
+
 
 server.listen(80);
 
+function openChannel (channel) {
+	channels[channel] = {
+		socket : null,
+		members : {}
+	};
+	channels[channel].socket = io.of("/channels/" + channel);
+	channels[channel].socket.on('connect', function (socket) {
+		var socketId = socket.id;
+		channels[channel].members[socketId] = {
+			uploadTimer : 0,
+			ttl : 30,
+			pings : [],
+			uploads : 0,
+			updateTtl : function () {
+				this.ttl = this.ttl - (this.pings.length * 5);
+				if (this.ttl < 0) {
+					// kill member if ttl is below 0
+					io.sockets.emit(channel + "members", Object.getOwnPropertyNames(channels[channel].members).length);
+					delete this;
+				}
+			}
+		};
+
+		channels[channel].socket.emit(channel + "members", Object.getOwnPropertyNames(channels[channel].members).length);
+
+		var pinger = setInterval(function () {
+			var testString = randomString(16);
+			channels[channel].members[socketId].pings.push(testString);
+			socket.emit('ping', {testString:testString});
+		},5000);
+
+		socket.on('pingback', function (data) {
+			channels[channel].members[socketId].pings = channels[channel].members[socketId].pings.filter(function (item) {
+				return item != data.testString;
+			});
+			channels[channel].members[socketId].updateTtl();
+		});
+		socket.on('disconnect', function () {
+			clearInterval(pinger);
+			delete channels[channel].members[socketId];
+			channels[channel].socket.emit(channel + "members", Object.getOwnPropertyNames(channels[channel].members).length);
+		});
+	});
+}
 
 
-function processImage (file, cb) {
+function randomString (length) {
+	var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUFWXYZ0123456789";
+	var string = "";
+	for (var i=0; i < length; i++) {
+		string += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+	return string;
+}
+
+
+function processImage (file, watermark, cb) {
 	var image = [];
 	var row = [];
 	var rowPart = [];
 	var processDone = new events.EventEmitter();
 	gm(file)
-		.size(function (err, size) {
-			if (err) {
-				console.log(err);
-				cb(err);
-			} else {
-				this.resize(130, 130, "!");
-				this.rotate("#FFFFFF",-90);
-				this.flip();
-				this.setFormat('RGB');
-				this.stream(function (err, stdout, stderr) {
-					stdout.on('data', function (data) {
-//						cb(null, data);
-						var rawData = JSON.stringify(data);
-						var rawDataArr = JSON.parse(rawData);
-						while (rawDataArr.length > 0) {
-							if (row.length == 130) {
-								image.push(row);
-								row = [];
-							}
-							var pixel = rawDataArr.splice(0,3);
-//							if (pixel.length != 3) {
-							while (pixel.length > 0) {
-								rowPart.push(pixel.shift());
-								if (rowPart.length == 3) {
-									row.push(rowPart);
-									rowPart = [];
-								}
-							}
-//							}
+		.resize(130, 130, "!")
+		.font("./cour.ttf")
+		.fontSize(12)
+		.fill("rgba(0,0,0,0.1)")
+		.drawText(2, 125, watermark)
+		.fill("rgba(255,255,255,0.1)")
+		.drawText(3, 126, watermark)
+		.rotate("#FFFFFF",-90)
+		.setFormat('RGB')
+		.stream(function (err, stdout, stderr) {
+			stderr.pipe(process.stderr);
+			stdout.on('data', function (data) {
+				var rawData = JSON.stringify(data);
+				var rawDataArr = JSON.parse(rawData);
+				while (rawDataArr.length > 0) {
+					if (row.length == 130) {
+						image.push(row);
+						row = [];
+					}
+					var pixel = rawDataArr.splice(0,3);
+					while (pixel.length > 0) {
+						rowPart.push(pixel.shift());
+						if (rowPart.length == 3) {
+							row.push(rowPart);
+							rowPart = [];
 						}
-						if (image.length == 130-1) {
-							image.push(row);
-							if (image.length == 130 && image[130 - 1].length == 130) {
-								processDone.emit('done');
-							}
-						}
-					});
-					processDone.on('done', function () {
-						// push each line from the image
-						// image should be 130x130 array
-						for (var i = image.length-1; i >= 0; i--) {
-							// start at the end of the image and work our way back
-							var line = [];
-							for (var j = 0; j < image[i].length; j++) {
-								// but start at the top of each column
-								line.push(image[i][j]);
-							}
-							cb(null, encodeLine(line));
-						}
-					});
-				});
-			}
+					}
+				}
+				if (image.length == 130-1) {
+					image.push(row);
+					if (image.length == 130 && image[130 - 1].length == 130) {
+						processDone.emit('done');
+					}
+				}
+			});
+			processDone.on('done', function () {
+				// push each line from the image
+				// image should be 130x130 array
+				for (var i = image.length-1; i >= 0; i--) {
+					// start at the end of the image and work our way back
+					var line = [];
+					for (var j = 0; j < image[i].length; j++) {
+						// but start at the top of each column
+						line.push(image[i][j]);
+					}
+					cb(null, encodeLine(line));
+				}
+			});
 		});
 }
 
 function encodeLine (line) {
-	// Line data: [ n,[r,g,b] ], [ [ [pos1, pos2, posN...],[r,g,b] ] ]
+	// Line data: [ [ [pos1, pos2, posN...],[r,g,b] ] ]
 	var colors = {};
 	var encLine = [];
 	for (var i=0; i < line.length; i++) {
--- js/controllers.js
+++ js/controllers.js
@@ -2,60 +2,81 @@ var imgtickrControllers = angular.module('imgtickrControllers', []);
 
 imgtickrControllers.controller('mainCtrl', ['$scope', 'api', function ($scope, api) {
 	$scope.upload = function (formData, channel) {
-		api.upload(formData, channel);
+		api.upload(formData, channel, $scope.handle);
 	};
+
+	$scope.handle = randomString(13);
 
 	$scope.openChannels = {};
 	$scope.openChannelNames = [];
+	$scope.openChannelMembers = {};
 
-	$scope.openChannel = function (channel) {
-		// TODO: check that channel isn't already open.
-		console.log(channel);
-		api.openChannel(channel).success(function () {
-			var socket = io.connect('http://' + window.location.host + '/channels/' + channel);
-			socket.on('connect', function () {
-				// create channel element
-				$scope.openChannelNames.push(channel);
-				console.log($scope.openChannelNames);
-				$scope.$apply();
-			});
-			socket.on('disconnect', function () {
-				// destroy the channel element, but with some safeguards so random disconnects don't fuck with users.
-				$scope.openChannelNames.splice($scope.openChannelNames.indexOf(channel));
-			});
-			socket.on(channel, function (data) {
-//				console.log(data);
-				$scope.$broadcast(channel, decodeLine(data));
-			});
-			$scope.openChannels[channel] = socket;
-		});
+	$scope.channels = {
+		open : function (channel) {
+			if ($scope.openChannelNames.indexOf(channel) == -1) {
+				api.openChannel(channel).success(function (data) {
+					var socket = io.connect('http://' + window.location.host + '/channels/' + channel);
+					socket.on('connect', function () {
+						// create channel element
+						$scope.openChannelNames.push(channel);
+						// TODO: make openChannelNames array reflect openChannels object via watch / Object.getOwnPropertyNames()
+						$scope.$apply();
+					});
+					socket.on('disconnect', function () {
+						// destroy the channel element, but with some safeguards so random disconnects don't fuck with users.
+						$scope.openChannelNames.splice($scope.openChannelNames.indexOf(channel));
+					});
+					socket.on(channel + "line", function (data) {
+						$scope.$broadcast(channel + "line", decodeLine(data));
+					});
+					socket.on(channel + 'members', function (data) {
+						$scope.openChannels[channel].members = data;
+						$scope.$apply();
+					});
+					socket.on('ping', function (data) {
+						socket.emit('pingback', {
+							testString : data.testString
+						});
+					});
+					$scope.openChannels[channel] = {
+						socket : socket,
+						members : data.members
+					};
+					$scope.channels.toOpen = "";
+				});
+			}
+		},
+		toOpen : "",
+		search : function () {
+			// TODO: Search channels
+		}
 	};
 
-	$scope.openChannel('public');
+	$scope.channels.open('public');
+
+	$scope.$watch('channelToOpen', function () {
+		if ($scope.channels.toOpen) {
+			$scope.channels.toOpen = $scope.channels.toOpen.replace(/\W+/g, "", "g").trim();
+		}
+	});
 
 	function decodeLine (lineData) {
-		// Line data: [ n,[r,g,b] ], [ [ [pos1, pos2, posN...],[r,g,b] ] ]
+		// Line data: [ [ [pos1, pos2, posN...],[r,g,b] ] ]
 		var line = [];
 		for (var i=0; i < lineData.length; i++) {
-//			console.log(lineData.length);
-//			if (typeof lineData[i][0] == 'array') {
-				// we're getting specific position values for this color.
-				for (var posIndex = 0; posIndex < lineData[i][0].length; posIndex++) {
-					line[lineData[i][0][posIndex]] = lineData[i][1];
-				}
-//			} else if (typeof lineData[i][0] == 'number') {
-				// we're getting a pixel repeated n times in a row, starting from i
-//				var pos = Number(i);
-//				var count = 0;
-//				do {
-//					line[pos] = lineData[i][0];
-//					pos++;
-//					count++;
-//				} while (count < lineData[i][0]);
-//			}
+			for (var posIndex = 0; posIndex < lineData[i][0].length; posIndex++) {
+				line[lineData[i][0][posIndex]] = lineData[i][1];
+			}
 		}
-//		console.log(lineData);
-//		console.log(line);
 		return line;
+	}
+
+	function randomString (length) {
+		var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUFWXYZ0123456789";
+		var string = "";
+		for (var i=0; i < length; i++) {
+			string += chars.charAt(Math.floor(Math.random() * chars.length));
+		}
+		return string;
 	}
 }]);
